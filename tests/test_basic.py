5461c5d2b6a90a5b28db2aa80ec0376d1bd67a08
Biplob_Gautam <biplobgautam.work@gmail.com>
2025-09-22 23:03:50 +0530

    Removed the simulation and testing files

--- tests/test_basic.py
+++
@@ -1,6 +0,0 @@
-from simulation.core import UAV
-
-def test_uav_step():
-    u = UAV("U1", [0,0], [1,0])
-    u.step()
-    assert u.pos == [1,0]
--- tests/test_graph.py
+++
@@ -1,8 +0,0 @@
-from simulation.graph import build_airspace_graph, shortest_path, add_no_fly_zone
-
-def test_shortest_path():
-    G = build_airspace_graph()
-    path = shortest_path(G, "A", "C")
-    assert path[0] == "A" and path[-1] == "C"
-    add_no_fly_zone(G, [("A","C")])
-    print(path)
