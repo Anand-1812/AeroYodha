baf98f2ffcf27071d888be73fc21dc12cba05e96
BT01257 <devdwuti@gmail.com>
2025-09-24 00:38:46 +0530

    Add: drone random movement on random values

--- frontend/src/components/map/UAVMap.jsx
+++ frontend/src/components/map/UAVMap.jsx
@@ -1,45 +1,87 @@
-import React from 'react';
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
-import L from 'leaflet';
-import herodrone from "../../../public/hero-drone.png"
+import React, { Component } from "react";
+import {
+  MapContainer,
+  TileLayer,
+  Popup,
+  Rectangle,
+} from "react-leaflet";
+import L from "leaflet";
+import herodrone from "../../../public/hero-drone.png";
+import ReactLeafletDriftMarker from "react-leaflet-drift-marker";
 
-// Fix default marker icon issue in React
-// delete L.Icon.Default.prototype._getIconUrl;
-// L.Icon.Default.mergeOptions({
-//   iconRetinaUrl:
-//     'https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon-2x.png',
-//   iconUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png',
-//   shadowUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png',
-// });
+function gen_position() {
+  const minLat = 28.4, maxLat = 28.88;
+  const minLng = 76.84, maxLng = 77.35;
+  return {
+    lat: parseFloat((Math.random() * (maxLat - minLat) + minLat).toFixed(6)),
+    lng: parseFloat((Math.random() * (maxLng - minLng) + minLng).toFixed(6)),
+  };
+}
 
 const uavIcon = new L.Icon({
-  iconUrl: herodrone, // path to your image
-  iconSize: [40, 40],           // size of the icon
-  iconAnchor: [20, 40],         // point of the icon which will correspond to marker's location
-  popupAnchor: [0, -40]         // where the popup should open relative to the iconAnchor
+  iconUrl: herodrone,
+  iconSize: [40, 40],
+  iconAnchor: [20, 40],
+  popupAnchor: [0, -40],
 });
 
-
 export default function basic_map() {
+  const rectangle1 = [
+    [28.6, 77.19],
+    [28.64, 77.23],
+  ];
+  const rectangle2 = [
+    [28.52, 77.16],
+    [28.56, 77.2],
+  ];
+  const redOptions = { color: "red" };
+
+  class SampleComp extends Component {
+    state = { latlng: gen_position() };
+
+    componentDidMount() {
+      this.interval = setInterval(() => {
+        this.setState({ latlng: gen_position() });
+      }, 3000);
+    }
+
+    componentWillUnmount() {
+      clearInterval(this.interval);
+    }
+
+    render() {
+      return (
+        <ReactLeafletDriftMarker
+          position={this.state.latlng}
+          duration={1000}
+          icon={uavIcon}
+        >
+          <Popup>Hello Delhi!</Popup>
+        </ReactLeafletDriftMarker>
+      );
+    }
+  }
+
   return (
-    <div className='d-flex' style={{ height: '100vh', width: '100%' }}>
-      <MapContainer className='mapcontainer'
-       center={[28.6139, 77.209]}
-       zoom={13} 
-       style={{ height: '100%', width: '100%'}} 
-       zoomControl={false}
-       scrollWheelZoom={false}
-       doubleClickZoom={false}
-       touchZoom={false}
-       dragging={false}
-       >
+    <div className="d-flex" style={{ height: "100vh", width: "100%" }}>
+      <MapContainer
+        className="mapcontainer"
+        center={[28.6139, 77.209]}
+        zoom={10}
+        style={{ height: "100%", width: "100%" }}
+        zoomControl={false}
+        scrollWheelZoom={false}
+        doubleClickZoom={false}
+        touchZoom={false}
+        dragging={false}
+      >
         <TileLayer
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           attribution="&copy; OpenStreetMap contributors"
         />
-        <Marker position={[28.6139, 77.209]} icon={uavIcon}>
-          <Popup>Hello Delhi!</Popup>
-        </Marker>
+        <SampleComp />
+        <Rectangle bounds={rectangle1} pathOptions={redOptions} />
+        <Rectangle bounds={rectangle2} pathOptions={redOptions} />
       </MapContainer>
     </div>
   );
