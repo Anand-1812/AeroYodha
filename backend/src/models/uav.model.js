c2dc4030c187c4b84dc8935343e3e65d4db74557
Anand-1812 <anand.work2005@gmail.com>
2025-10-06 15:06:07 +0530

    feat: add no-fly-zone for geo fencing

--- backend/src/models/uav.model.js
+++ backend/src/models/uav.model.js
@@ -1,22 +1,29 @@
 import mongoose, { Schema } from "mongoose";
 
+// UAV model
 const uavSchema = new Schema({
-  id: { type: Number, required: true },       // UAV id from sim
+  id: { type: Number, required: true },
   x: { type: Number, required: true },
   y: { type: Number, required: true },
   start: { type: String, required: true },
   goal: { type: String, required: true },
   reached: { type: Boolean, default: false },
-  path: [{ type: String }]                    // array of node ids
+  path: [{ type: String }]
 });
 
 const stepSchema = new Schema(
   {
-    step: { type: Number, required: true },   // simulation timestep
-    uavs: [uavSchema]                         // array of UAV states
+    step: { type: Number, required: true },
+    uavs: [uavSchema],
+
+    // for geo fencing
+    noFlyZones: [{ type: String }]
   },
-  { timestamps: true } // adds createdAt & updatedAt
+  { timestamps: true }
 );
 
+// Auto-delete after 2 mins
 stepSchema.index({ createdAt: 1 }, { expireAfterSeconds: 120 });
+
 export const UAV = mongoose.model("StepSnapshot", stepSchema);
+
