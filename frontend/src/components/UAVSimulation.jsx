31a9e5a0cd2e767da1ddc77c65aebf574d56f4e1
BT01257 <devdwuti@gmail.com>
2025-10-07 12:13:25 +0530

    fixed: buttons working

--- frontend/src/components/UAVSimulation.jsx
+++ frontend/src/components/UAVSimulation.jsx
@@ -1,30 +1,46 @@
-import React from "react";
-import { useSimulation } from "./simulation/useSimulation";
+import React, { useState } from "react";
 import Controls from "./controls/Controls";
-import UAVMap from "./map/UAVMap";
+import BasicMap from "./map/UAVMap";
 
-const UAVSimulation = () => {
-  const sim = useSimulation();
-  const { uavs, geofences, running, setRunning, stop, refreshData } = sim;
+export default function UAVSimulation() {
+  const [running, setRunning] = useState(false);
+  const [uavs, setUavs] = useState([]);
+  const [uavCount, setUavCount] = useState(1);
+
+  const generateRandomUavs = (count = 1) => {
+    const arr = Array.from({ length: count }).map((_, i) => ({
+      id: i,
+      speed: 80 + Math.random() * 50,
+      trajectory: Array.from({ length: 30 }).map((_, j) => [
+        Math.random() * 30,
+        Math.random() * 30,
+      ]),
+    }));
+    setUavs(arr);
+  };
+
+  const handleAddUavs = () => generateRandomUavs(Number(uavCount));
+  const refreshData = () => generateRandomUavs(uavCount);
+  const stop = () => {
+    setRunning(false);
+    setUavs([]);
+  };
 
   return (
-  <div className="container-fluid h-100">
-    <div className="row h-100 g-0">
-      <div className="col-9">
-        <UAVMap uavs={uavs} geofences={geofences} />
-      </div>
-      <div className="col-3">
-        <Controls
-          running={running}
-          setRunning={setRunning}
-          stop={stop}
-          refreshData={refreshData}
-          uavs={uavs}
-        />
+    <div style={{ display: "flex", flexDirection: "column", height: "100vh" }}>
+      <Controls
+        running={running}
+        setRunning={setRunning}
+        stop={stop}
+        refreshData={refreshData}
+        uavs={uavs}
+        uavCount={uavCount}
+        setUavCount={setUavCount}
+        handleAddUavs={handleAddUavs}
+      />
+      <div style={{ flex: 1 }}>
+        <BasicMap running={running} uavs={uavs} />
       </div>
     </div>
-  </div>
-);
-};
-
-export default UAVSimulation;
+  );
+}
