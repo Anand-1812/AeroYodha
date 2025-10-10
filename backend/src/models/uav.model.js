7ae7af83949ed02c596052476f152ae9aa325d4f
Biplob_Gautam <biplobgautam.work@gmail.com>
2025-10-10 12:58:50 +0530

    remove time merge conflict

--- backend/src/models/uav.model.js
+++ backend/src/models/uav.model.js
@@ -25,12 +25,6 @@ const stepSchema = new Schema(
   { timestamps: true }
 );
 
-// Automatically delete after 2 mins (like before)
-<<<<<<< HEAD
 stepSchema.index({ createdAt: 1 }, { expireAfterSeconds: 3600 });
-=======
-stepSchema.index({ createdAt: 1 }, { expireAfterSeconds: 360 });
->>>>>>> simulation
-
 export const StepSnapshot = mongoose.model("StepSnapshot", stepSchema);
 
